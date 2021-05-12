d3f3718e3dc29554b88dd500ee50ba9b28c618a9
ChengBo <1194892805@qq.com>
2021-05-12 16:30:10 +0800

    joint_regression

--- my_config/1origin_cfg/test_fcos_center_r50_caffe_fpn_gn-head_1x_coco.py
+++ my_config/1origin_cfg/test_fcos_center_r50_caffe_fpn_gn-head_1x_coco.py
@@ -1,40 +1,3 @@
-dataset_type = 'CocoDataset'
-data_root = 'dataset/COCO2017/'
-img_norm_cfg = dict(
-    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(
-        type='Normalize',
-        mean=[102.9801, 115.9465, 122.7717],
-        std=[1.0, 1.0, 1.0],
-        to_rgb=False),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(
-                type='Normalize',
-                mean=[102.9801, 115.9465, 122.7717],
-                std=[1.0, 1.0, 1.0],
-                to_rgb=False),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
-        ])
-]
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
@@ -180,5 +143,3 @@ model = dict(
         score_thr=0.05,
         nms=dict(type='nms', iou_threshold=0.5),
         max_per_img=100))
-work_dir = './work_dirs/fcos_center_r50_caffe_fpn_gn-head_1x_coco'
-gpu_ids = [1]
